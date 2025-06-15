b9640694b51be7a81ea96233456f1043f1a5d4fd
gayatri-011 <gayatri.juneja04@gmail.com>
2025-06-15 21:30:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,20 +3,18 @@ import pandas as pd
 import gspread
 import streamlit as st
 from google.oauth2 import service_account
-import base64
 
 @st.cache_data(ttl=600)
 def load_data(selected_sheet):
-    # Load service account credentials from Streamlit secrets
-    encoded_credentials = st.secrets["GOOGLE_CREDENTIALS_BASE64"]
-    credentials_info = json.loads(base64.b64decode(encoded_credentials).decode('utf-8'))
+    # Load service account credentials directly from Streamlit secrets
+    credentials_info = st.secrets["GOOGLE_CREDENTIALS"]
     credentials = service_account.Credentials.from_service_account_info(credentials_info)
     
     # Authorize gspread
     client = gspread.authorize(credentials)
 
     # Open your Google Sheet
-    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1sjfGn--p6WGLPpsE5TdvNSHiuoFyy7IT6ACwlsRgwgg/edit?usp=sharing")
+    sheet = client.open("Monthly Analysis and Prediction")
 
     # Load selected sheet dynamically
     worksheet = sheet.worksheet(selected_sheet)
